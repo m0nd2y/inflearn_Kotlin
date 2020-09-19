c9183e04651f463c23fe01e8d1bf4387fef77bc7
m0nd2y <ldj6192@gmail.com>
2020-09-20 08:48:08 +0900

    commit

--- app/src/main/java/com/example/myapplication/Main.kt
+++ app/src/main/java/com/example/myapplication/Main.kt
@@ -1,8 +1,15 @@
 package com.example.myapplication
 
 fun main() {
-    val items = listOf(1, 2, 3, 4, 5) //Int
+    val items = listOf(1, 2, 3, 4, 5) //불변
+    val items2 = arrayListOf(1, 2, 3, 4, 5) //변경 가능
 
-    print(items)
-    print("1234")
+    items2.add(6)
+    items2.remove(1)
+
+    items2[0] = 10
+
+
+    print(items2)
+
 }
