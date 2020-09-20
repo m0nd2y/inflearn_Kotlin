ccfc476495ca886577a175728e83255ca211d0cc
m0nd2y <ldj6192@gmail.com>
2020-09-20 09:07:54 +0900

    commit

--- app/src/main/java/com/example/myapplication/Main.kt
+++ app/src/main/java/com/example/myapplication/Main.kt
@@ -8,5 +8,7 @@ fun main() {
         2 -> print("2입니다")
         3,4,5 -> print("3이나 4나 5입니다")
         in 6..20 -> print("6부터 20 사이의 값입니다")
+        !in 8..10 -> print("정답")
+        else -> print("else")
     }
 }
