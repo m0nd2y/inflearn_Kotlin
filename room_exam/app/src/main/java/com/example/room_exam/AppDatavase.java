821102ca8ebf3c249d1f1bb5a86d48d5cfe4e907
m0nd2y <ldj6192@gmail.com>
2020-09-20 14:46:02 +0900

    commit

--- room_exam/app/src/main/java/com/example/room_exam/AppDatavase.java
+++
@@ -1,8 +0,0 @@
-package com.example.room_exam;
-
-import androidx.room.Database;
-import androidx.room.RoomDatabase;
-
-@Database(entities = {Todo.class})
-public abstract class AppDatavase extends RoomDatabase {
-}
--- room_exam/app/src/main/java/com/example/room_exam/MainActivity.java
+++ room_exam/app/src/main/java/com/example/room_exam/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        AppDatabase db = Room.databaseBuilder(this, AppDatabase.class, "todo-db").build();
 
     }
 }
