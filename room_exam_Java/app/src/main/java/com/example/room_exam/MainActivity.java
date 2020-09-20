bd043d94862368a4ae1421c8b7cecd4616cbe737
m0nd2y <ldj6192@gmail.com>
2020-09-20 21:51:33 +0900

    commit

--- room_exam_Java/app/src/main/java/com/example/room_exam/MainActivity.java
+++ room_exam_Java/app/src/main/java/com/example/room_exam/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-
+ 
         binding.setLifecycleOwner(this);
 
         MainViewModel viewModel = ViewModelProviders.of(this).get(MainViewModel.class);
