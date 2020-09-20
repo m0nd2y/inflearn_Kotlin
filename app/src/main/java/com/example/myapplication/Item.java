f4000164245164fc68f490f17aa119deafd9cfdc
m0nd2y <ldj6192@gmail.com>
2020-09-20 09:48:00 +0900

    commit

---
+++ app/src/main/java/com/example/myapplication/Item.java
@@ -0,0 +1,39 @@
+package com.example.myapplication;
+
+import java.util.Objects;
+
+class Item {
+    private String name;
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Item item = (Item) o;
+        return Objects.equals(name, item.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}
