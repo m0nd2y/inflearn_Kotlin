de2d7f9ff66475e755ed5e32dfad4edd25b415d3
m0nd2y <ldj6192@gmail.com>
2020-09-20 09:02:34 +0900

    commit

--- app/src/main/java/com/example/myapplication/Main.kt
+++ app/src/main/java/com/example/myapplication/Main.kt
@@ -1,10 +1,7 @@
 package com.example.myapplication
 
 fun main() {
-    var x = 2
-    if (x%2 == 0) {
-        print("짝")
+    for (i in 0..9) {
+        print(i)
     }
-
-    val isEven =  if(x % 2 == 0) "짝" else "홀"
 }
