5b33ac0d6268d1a1fdb60cac363f5bc2aa7698b6
m0nd2y <ldj6192@gmail.com>
2020-09-20 16:44:15 +0900

    commit

--- room_exam_kotlin/app/src/main/java/com/example/room_exam_kotlin/MainActivity.kt
+++ room_exam_kotlin/app/src/main/java/com/example/room_exam_kotlin/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.room_exam_kotlin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import androidx.room.Room
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -11,11 +13,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val db = Room.databaseBuilder(
-                applicationContext,
-                Appdatabase::class.java, "db"
-        ).build()
-
+        val viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
         db.todoDao().getAll().observe(this, Observer { todos ->
             result_text.text = todos.toString()
         })
--- room_exam_kotlin/app/src/main/java/com/example/room_exam_kotlin/MainViewModel.kt
+++ room_exam_kotlin/app/src/main/java/com/example/room_exam_kotlin/MainViewModel.kt
@@ -2,8 +2,21 @@ package com.example.room_exam_kotlin
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.room.Room
 
 class MainViewModel(application: Application): AndroidViewModel(application) {
+    private val db = Room.databaseBuilder(
+        application,
+        Appdatabase::class.java, "db"
+    ).build()
 
+    fun getAll(): LiveData<List<Todo>> {
+        return db.todoDao().getAll()
+    }
+
+    fun insert (todo: Todo) {
+        db.todoDao().insert(todo)
+    }
 
 }
