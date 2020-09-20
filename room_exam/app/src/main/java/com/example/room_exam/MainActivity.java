c625f0da997ea6d5474d40ac636875475d373f7a
m0nd2y <ldj6192@gmail.com>
2020-09-20 14:47:30 +0900

    commit

--- room_exam/app/src/main/java/com/example/room_exam/MainActivity.java
+++ room_exam/app/src/main/java/com/example/room_exam/MainActivity.java
@@ -4,15 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.room.Room;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText mTodoEditText;
+    private TextView mResultTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        AppDatabase db = Room.databaseBuilder(this, AppDatabase.class, "todo-db").build();
+        mTodoEditText = findViewById(R.id.todo_edit);
+        mResultTextView = fileList(R.id.result_text);
 
+        AppDatabase db = Room.databaseBuilder(this, AppDatabase.class, "todo-db").build();
+        
     }
 }
