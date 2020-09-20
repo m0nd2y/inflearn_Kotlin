75c1bb6fef48cf6fef81300106e836669c087692
m0nd2y <ldj6192@gmail.com>
2020-09-20 09:12:18 +0900

    commit

--- app/src/main/java/com/example/myapplication/Main.kt
+++ app/src/main/java/com/example/myapplication/Main.kt
@@ -1,14 +1,15 @@
 package com.example.myapplication
 
 fun main() {
-    val x = 1
+    // Person person = new Person();
 
-    when(x) {
-        1 -> print("1입니다")
-        2 -> print("2입니다")
-        3,4,5 -> print("3이나 4나 5입니다")
-        in 6..20 -> print("6부터 20 사이의 값입니다")
-        !in 8..10 -> print("정답")
-        else -> print("else")
+    var person = Person("홍길동")
+    person.name = "한석봉"
+    print(person.name)
+}
+
+class Person(var name: String) {
+    init {
+        print(name)
     }
 }
